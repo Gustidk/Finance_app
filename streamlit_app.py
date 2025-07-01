f5ca37efd1d160d7c2edde9cc736631cda826680
Gustidk <94616140+Gustidk@users.noreply.github.com>
2025-07-01 15:04:05 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -69,7 +69,7 @@ if response_is.status_code == 200 and response_bs.status_code == 200:
             height=500,
             legend=dict(
                 orientation="h",            # Horisontal
-                yanchor="bottom",           # Fastgør bunden af legenden
+                yanchor="top",           # Fastgør bunden af legenden
                 y=1.04,                     # Lidt over selve figuren
                 xanchor="center",           # Centreret på x-aksen
                 x=0.5
@@ -90,7 +90,7 @@ if response_is.status_code == 200 and response_bs.status_code == 200:
             height=400,
             legend=dict(
                 orientation="h",            # Horisontal
-                yanchor="bottom",           # Fastgør bunden af legenden
+                yanchor="top",           # Fastgør bunden af legenden
                 y=1.04,                     # Lidt over selve figuren
                 xanchor="center",           # Centreret på x-aksen
                 x=0.5
@@ -114,7 +114,7 @@ if response_is.status_code == 200 and response_bs.status_code == 200:
             height=500,
             legend=dict(
                 orientation="h",            # Horisontal
-                yanchor="bottom",           # Fastgør bunden af legenden
+                yanchor="top",           # Fastgør bunden af legenden
                 y=1.04,                     # Lidt over selve figuren
                 xanchor="center",           # Centreret på x-aksen
                 x=0.5
@@ -167,7 +167,7 @@ if response_cf.status_code == 200:
             height=500,
             legend=dict(
                 orientation="h",            # Horisontal
-                yanchor="bottom",           # Fastgør bunden af legenden
+                yanchor="top",           # Fastgør bunden af legenden
                 y=1.04,                     # Lidt over selve figuren
                 xanchor="center",           # Centreret på x-aksen
                 x=0.5
